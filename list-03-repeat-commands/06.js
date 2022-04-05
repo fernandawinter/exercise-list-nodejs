2e081dcfbbf95c76493716ae46a8b2e8701a5f1b
Fernanda Winter <fernanda.winter@hotmail.com>
2022-04-05 18:39:17 -0300

    list 03: exercise 06 - fixed and with 1 more test

--- list-03-repeat-commands/06.js
+++ list-03-repeat-commands/06.js
@@ -1,11 +1,14 @@
 function getTheAverage(array) {
     let sum = 0;
-    let average = 0;
+
+    if(array.length == 0) {
+        return 0;
+    }
 
     for(let i = 0; i < array.length; i++) {
         sum += array[i];
-        average = sum / 10;
     }
+    const average = sum / array.length;
     return average;
 }
 module.exports = getTheAverage;
--- list-03-repeat-commands/06.test.js
+++ list-03-repeat-commands/06.test.js
@@ -1,5 +1,9 @@
 const getTheAverage = require('./06');
 
-test('should get the average of the numbers 4, 3, 2, 7, 9, 11, 13, 15, 5, 6', () => {
+test('should get the average of the numbers 4, 3, 2, 7, 9, 11, 13, 15, 5, 6, that is 7.5', () => {
     expect(getTheAverage([4, 3, 2, 7, 9, 11, 13, 15, 5, 6])).toEqual(7.5);
+});
+
+test('should get the average of an empty array, that is 0', () => {
+    expect(getTheAverage([])).toEqual(0);
 });
